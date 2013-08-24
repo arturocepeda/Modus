581c83c013b2e2bc0d120fb13bdf6131bbca0671
Joshua Downer <joshua.downer@gmail.com>
2013-08-24 03:27:15 -0400

    Added support for different GCC versions.
    The issues here is that the flag used to specify the use of the 2011 standard
    change from version 4.6 (c++0x) to 4.7 (c++11). This change introduces a query
    to determine which version of the compiler is being used and sets the flag
    accordingly.

--- lib/Linux/buildlib.sh
+++ lib/Linux/buildlib.sh
@@ -2,9 +2,29 @@
 cp ./../../src/*.* ./
 cp ./../../include/*.* ./
 
-g++ -std=c++0x -w -c *.cpp
+# Query for the version of GCC that is being used
+VERSION=$(gcc -dumpversion)
+
+# Split the version string using the periods and create an array in order to
+# extract the major and minor version numbers
+VERSION=(${VERSION//./ })
+MAJOR=${VERSION[0]}
+MINOR=${VERSION[1]}
+
+# Set the C++ standard based upon the version of GCC available
+if [ ${MAJOR} -eq 4 ] && [ ${MINOR} -gt 6 ]
+then
+  echo ${MAJOR} ${MINOR}
+  CXXSTD="c++11"
+else
+  CXXSTD="c++0x"
+fi
+
+# Compile and link
+g++ -std=${CXXSTD} -w -c *.cpp
 ar rvs modus.a *.o
 
+# Clean up
 rm -f *.o
 rm -f *.cpp
 rm -f *.h
